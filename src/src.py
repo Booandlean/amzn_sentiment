3f56d71de20b80293c462362724e68e71845494b
Adam Roth <aroth605@gmail.com>
2020-12-19 16:42:24 -0800

    nltk stuff

---
+++ src/src.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+from nltk.tokenize import word_tokenize
+import nltk
+from nltk.tokenize import regexp_tokenize, word_tokenize, RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet
+from nltk import pos_tag
+from nltk.stem import WordNetLemmatizer 
