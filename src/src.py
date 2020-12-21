7d31a766392911532b391aec5aeed47d703c9d4f
Adam Roth <aroth605@gmail.com>
2020-12-21 15:36:03 -0800

    boop

--- src/src.py
+++ src/src.py
@@ -7,3 +7,46 @@ from nltk.corpus import stopwords
 from nltk.corpus import wordnet
 from nltk import pos_tag
 from nltk.stem import WordNetLemmatizer 
+
+#from last NLP project, written with Amanda Potter 
+def doc_preparer(doc, stop_words=sw):
+    '''
+    
+    :param doc: a document from the corpus 
+    :return: a document string with words which have been 
+            lemmatized, 
+            parsed for stopwords, 
+            made lowercase,
+            and stripped of punctuation and numbers.
+    '''
+    
+    regex_token = RegexpTokenizer(r"([a-zA-Z]+(?:’[a-z]+)?)")
+    doc = regex_token.tokenize(doc)
+    doc = [word.lower() for word in doc]
+    doc = [word for word in doc if word not in stop_words]
+    doc = pos_tag(doc)
+    doc = [(word[0], get_wordnet_pos(word[1])) for word in doc]
+    lemmatizer = WordNetLemmatizer() 
+    doc = [lemmatizer.lemmatize(word[0], word[1]) for word in doc]
+    return ' '.join(doc)
+
+#from last NLP project, written with Amanda Potter 
+from nltk.corpus import wordnet
+from nltk import pos_tag
+from nltk.stem import WordNetLemmatizer 
+  
+
+def get_wordnet_pos(treebank_tag):
+    '''
+    Translate nltk POS to wordnet tags
+    '''
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
